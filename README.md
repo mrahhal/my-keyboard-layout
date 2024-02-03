f403b8d5e4e843c98a5aedf2e658926ff290d739
Mohammad Rahhal <m.r992@hotmail.com>
2024-02-03 17:15:46 +0100

    Add key overrides for numpad keys -> fn keys

--- README.md
+++ README.md
@@ -93,3 +93,4 @@ I'll try to keep this updated with my changes differing from Oryx layout.
 - Custom LED indicator lights depending on the active layers. This is because I have a number of language layers I rarely switch to, but these need to be low so that they can still access MO layers. This setup made the LED indicators useless for the most part. This change implements a custom lighting behavior for these LEDs to be more useful.
 - Key overrides:
   - SHIFT+BSPC -> DEL
+  - SHIFT+Numpad keys -> Fn keys (avoids the need for an even additional layer just for the fn keys to take the place of number keys)
--- keymaps/main/keymap.c
+++ keymaps/main/keymap.c
@@ -628,6 +628,20 @@ const key_override_t **key_overrides = (const key_override_t *[]){
     // SHIFT+BSPC -> DEL
     &ko_make_basic(MOD_MASK_SHIFT, LT(7,KC_BSPC), KC_DEL),
 
+    // SHIFT+Numpad -> Fn keys
+    &ko_make_basic(MOD_MASK_SHIFT, KC_KP_1, KC_F1),
+    &ko_make_basic(MOD_MASK_SHIFT, KC_KP_2, KC_F2),
+    &ko_make_basic(MOD_MASK_SHIFT, KC_KP_3, KC_F3),
+    &ko_make_basic(MOD_MASK_SHIFT, KC_KP_4, KC_F4),
+    &ko_make_basic(MOD_MASK_SHIFT, KC_KP_5, KC_F5),
+    &ko_make_basic(MOD_MASK_SHIFT, KC_KP_6, KC_F6),
+    &ko_make_basic(MOD_MASK_SHIFT, KC_KP_7, KC_F7),
+    &ko_make_basic(MOD_MASK_SHIFT, KC_KP_8, KC_F8),
+    &ko_make_basic(MOD_MASK_SHIFT, KC_KP_9, KC_F9),
+    &ko_make_basic(MOD_MASK_SHIFT, KC_KP_DOT, KC_F10),
+    &ko_make_basic(MOD_MASK_SHIFT, KC_KP_PLUS, KC_F11),
+    &ko_make_basic(MOD_MASK_SHIFT, KC_KP_MINUS, KC_F12),
+
     // Null terminator is required at the end.
     NULL
 };
