04e108eab5d6d7f5b025dbea7fc73383ec966ddd
Mohammad Rahhal <m.r992@hotmail.com>
2024-02-06 11:12:39 +0100

    Update

--- keymaps/main/keymap.c
+++ keymaps/main/keymap.c
@@ -25,10 +25,10 @@ enum tap_dance_codes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_voyager(
     KC_ESCAPE,      KC_1,           KC_2,           KC_3,           KC_4,           KC_5,                                           KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_BSLS,        
-    KC_LEFT_GUI,    KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,                                           KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_MINUS,       
+    MT(MOD_LGUI, KC_TAB),KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,                                           KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_MINUS,       
     KC_LEFT_SHIFT,  KC_A,           KC_S,           KC_D,           KC_F,           KC_G,                                           KC_H,           KC_J,           KC_K,           KC_L,           KC_SCLN,        MEH_T(KC_QUOTE),
     KC_LEFT_CTRL,   KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,                                           KC_N,           KC_M,           KC_COMMA,       KC_DOT,         ALL_T(KC_SLASH),KC_LEFT_GUI,    
-                                                    MT(MOD_LALT, KC_TAB),LT(5,KC_SPACE),                                 LT(7,KC_ENTER), LT(9,KC_BSPC)
+                                                    KC_LEFT_ALT,    LT(5,KC_SPACE),                                 LT(7,KC_ENTER), LT(9,KC_BSPC)
   ),
   [1] = LAYOUT_voyager(
     TD(DANCE_0),    JP_ZHTG,        JP_KANA,        JP_MKANA,       KC_NO,          KC_NO,                                          KC_LANGUAGE_1,  KC_LANGUAGE_2,  KC_NO,          KC_NO,          KC_NO,          KC_NO,          
@@ -106,7 +106,7 @@ const uint16_t PROGMEM combo0[] = { KC_D, LT(7,KC_ENTER), LT(5,KC_SPACE), COMBO_
 const uint16_t PROGMEM combo1[] = { KC_J, LT(7,KC_ENTER), LT(5,KC_SPACE), COMBO_END};
 const uint16_t PROGMEM combo2[] = { KC_A, LT(7,KC_ENTER), LT(5,KC_SPACE), COMBO_END};
 const uint16_t PROGMEM combo3[] = { KC_G, LT(7,KC_ENTER), LT(5,KC_SPACE), COMBO_END};
-const uint16_t PROGMEM combo4[] = { LT(9,KC_BSPC), MT(MOD_LALT, KC_TAB), COMBO_END};
+const uint16_t PROGMEM combo4[] = { LT(9,KC_BSPC), KC_LEFT_ALT, COMBO_END};
 
 combo_t key_combos[COMBO_COUNT] = {
     COMBO(combo0, TO(2)),
